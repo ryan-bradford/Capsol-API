7f79481971e60941f8685263f6a4967bb91f7abb
Ryan Bradford <ryanbradford333@gmail.com>
2019-10-17 19:21:26 -0400

    Fix mysql login

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,19 @@
-module.exports = {
+const toExport = {
     name: 'default',
     type: 'mysql',
     host: 'localhost',
     port: 3306,
     username: 'root',
-    password: 'password',
     database: process.env.USE_TEST_DB ? 'SOLAR_TEST' : 'SOLAR',
     synchronize: true,
     entities: ['./src/entities/**/*.ts'],
     migrations: ['db/migration/*.js'],
     insecureAuth: true,
 }
+
+if (process.env.IS_TRAVIS === 'false') {
+    toExport.password = 'password';
+    console.log(toExport);
+}
+
+module.exports = toExport;
